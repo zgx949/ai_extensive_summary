8e134a6f373bdc42deb82f5a9882ff10bff3fd9c
zgx <1637349696@qq.com>
2023-04-04 09:59:26 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    title: 'AI知识库',
+    description: '小景哥哥带你上王者',
+    dest: './dist',
+    port: '80',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.jpg' }]
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        nav: [{
+            text: '关于我',
+            link: '/aboutme/'
+        }],
+        sidebar: {
+            '/guide/': [{
+                    title: '新手指南',
+                    collapsable: true,
+                    children: [
+                        '/guide/notes/one',
+                    ]
+                },
+                {
+                    title: '人工智能',
+                    collapsable: true,
+                    children: [
+                        '/guide/notes/two',
+                    ]
+                }
+            ]
+        },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+        searchMaxSuggestoins: 10,
+        serviceWorker: {
+            updatePopup: {
+                message: "有新的内容.",
+                buttonText: '更新'
+            }
+        },
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页 ！'
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: logo.jpg
+heroText: 欢迎来到AI知识库
+tagline: 人工智能不会取代人类，而是与人类共同发展和进步，帮助人类更好地应对各种挑战。
+actionText: 开始探索之旅 →
+actionLink: /guide/
+features:
+- title: 什么是人工智能？
+  details: 人工智能是一种模拟人类智能的技术，包括计算机视觉、自然语言处理和机器学习等方面。
+- title: 为什么人工智能如此重要？
+  details: 人工智能可以帮助人类在各个领域提高效率和创造力，包括医疗保健、交通运输和金融等。
+- title: 人工智能的应用领域
+  details: 人工智能可以应用于医疗保健、金融、交通运输、教育和安全等领域。其中包括自动驾驶汽车、语音识别、图像识别和智能家居等。
+footer: Hep Licensed | Copyright © 2021-present JasonJing
+---
